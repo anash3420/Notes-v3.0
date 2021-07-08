a79aa991c66f7d112d803b4d9b45056ddd95b7b5
Anash Shelat <anashshelat@gmail.com>
2021-07-08 17:35:04 +0530

    Updates

---
+++ precache-manifest.a0c906032f6a193579cbd858700bd773.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de9565e8f5901898f3c7b9550f9fcf6",
+    "url": "/Notes-v3.0/index.html"
+  },
+  {
+    "revision": "9204a8b21e9269a77cdb",
+    "url": "/Notes-v3.0/static/js/2.9cd6e5ed.chunk.js"
+  },
+  {
+    "revision": "cffc1324a9b7ee72b641",
+    "url": "/Notes-v3.0/static/js/main.f7dae4f0.chunk.js"
+  },
+  {
+    "revision": "8db578508342a183440f",
+    "url": "/Notes-v3.0/static/js/runtime-main.b29c947f.js"
+  }
+]);
