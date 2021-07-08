bb25435e40e3556c964c4db2ee2dc980d32436c3
Anash Shelat <anashshelat@gmail.com>
2021-07-08 17:26:41 +0530

    gitignore

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+
+function Header() {
+  return (
+    <header>
+      <h1><AcUnitIcon />Notes</h1>
+    </header>
+  );
+}
+
+export default Header;
